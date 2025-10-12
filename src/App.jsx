ffc8c94bf1b2af1e9a1cb974ddab9aa1c28c77bf
chanda-27 <chandabiswas27@gmail.com>
2025-10-12 10:03:10 +0600

    added some folder and some file

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <Header></Header>
+      <h1>Main content</h1>
+      <Footer></Footer>
+    </>
+  );
+}
+
+export default App;
